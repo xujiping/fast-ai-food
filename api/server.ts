30c6f6f9f005e19ad35719e89effe6be05388e34
xujiping <xjpdyx@hotmail.com>
2026-01-18 16:55:41 +0800

    chore: 删除项目所有文件以进行重构或终止项目

--- api/server.ts
+++
@@ -1,9 +0,0 @@
-import app from './app.js'
-
-const PORT = process.env.PORT || 3001
-const server = app.listen(PORT, () => { console.log(`Server ready on port ${PORT}`) })
-
-process.on('SIGTERM', () => { server.close(() => { process.exit(0) }) })
-process.on('SIGINT', () => { server.close(() => { process.exit(0) }) })
-
-export default app
